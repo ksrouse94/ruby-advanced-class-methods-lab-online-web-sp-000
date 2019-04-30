3d6bfa78b2a00bc27d6de10d8bc84f7c10a32d84
Kimberly Rouse <ksrouse94@gmail.com>
2019-04-30 22:52:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,7 +56,8 @@ class Song
   def self.new_from_filename(file)
     info = file.split("-")
     name = info[0]
-    artist = info[1].
+    artist = info[1]
+    artist_name = artist[-3]
     
     new_song = self.new
     new_song.name = name
